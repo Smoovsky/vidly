0f089385318fabf500120a529e6c15d6b9b34336
smokovsky <chenshang920@hotmail.com>
2018-08-06 01:57:58 +1000

    Add rental record domain model

---
+++ Vidly/Controllers/API/NewRentalController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.DTOs;
+
+namespace Vidly.Controllers.API
+{
+    public class NewRentalController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDTO newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+}
